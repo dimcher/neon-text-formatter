2cf5893618c315931e53dfe0836b5bcb250f1da6
Dima Chernyshev <dimcher@gmail.com>
2022-05-10 16:18:08 -0400

    lib split

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,197 +1,8 @@
+mod support;
+
 use neon::prelude::*;
 
-use regex::Regex;
-
-use std::{fs::File, io::{Read, Write}, {thread, collections::HashMap}};
-
-const EOL: &str = "\r\n";
-const COMMA: &str = ",";
-const QUOTE: char = '"';
-const THSIZE: usize = 3;
-
-struct Files {}
-
-impl Files {
-    fn new<'a>() -> HashMap<&'a str, &'a str> {
-        let mut hmap = HashMap::new();
-
-        hmap.insert("csv", ",");
-        hmap.insert("tsv", "\t");
-        hmap.insert("psv", "|");
-
-        hmap
-    }
-}
-
-fn minmax(word: &str) -> [char; 2] {
-    let w: Vec<char> = word.chars().collect();
-
-    [w[0], w[w.len()-1]]
-}
-
-fn file_type(file: &str) -> Box<String> {
-    let re = Regex::new(r"[^\.]+$").unwrap();
-    let mat = re.find(file).unwrap();
-    
-    Box::new(file[mat.start()..mat.end()].to_string())
-}
-
-fn file_delim<'a>(mode: &str, map: &HashMap<&str, &str>) -> Box<String> {
-    Box::new(map.get(mode).unwrap_or(&COMMA).to_string())
-}
-
-fn read_file<'a>(file: &str) -> Box<String> {
-    let mut file = File::open(file).unwrap();
-    let mut contents = String::new();
-
-    file.read_to_string(&mut contents).unwrap();
-
-    Box::new(contents)
-}
-
-fn write_file(file: &str, data: &str) -> usize {
-    let mut file = File::create(file).unwrap();
-    let data = data.as_bytes();
-    file.write_all(data).unwrap();
-
-    data.len()
-}
-
-fn auto_trim (mut text: String, beg: bool, end: bool) -> Box<String> {
-    let len: usize = text.len();
-
-    if len > 1 {
-        if beg {
-            text.remove(0);
-        }
-        if end {
-            text.pop();
-        }
-    }
-
-    Box::new(text)
-}
-
-fn parse_csv(lines: &Vec<String>) -> Box<Vec<Vec<String>>> {
-    let mut data: Vec<Vec<String>> = Vec::new();
-    let re = Regex::new(r#""{2}"#).unwrap();
-
-    for line in lines {
-        let ln = re.replace(line, String::from(QUOTE));
-
-        let parts = ln.split(COMMA).enumerate();
-        let last = parts.clone().count() - 1;
-
-        let mut words: Vec<String> = Vec::new();
-        let mut append: String = String::from("");
-
-        for (wi, wd) in parts {
-            let [mut min, mut max]: [char; 2] = [' ', ' '];
-
-            if wd.len() > 0 {
-                [min, max] = minmax(wd);
-            }
-
-            let beg: bool = min == QUOTE;
-            let end: bool = max == QUOTE;
-
-            if append.len() > 0 {
-                append.push_str(COMMA);
-                append.push_str(wd);
-                
-                if end {
-                    append = *auto_trim(append, true, true);
-                    words.push(append);
-                    append = String::new();
-                }
-                else if wi == last {
-                    words.push(*auto_trim(append.clone(), true, end));
-                }
-
-                continue;
-            }
-
-            if beg && !end && wi != last {
-                append = String::from(wd);
-                continue;
-            }
-
-            words.push(*auto_trim(String::from(wd), beg, end));
-        }
-        
-        data.push(words);
-    }
-
-    Box::new(data)
-}
-
-fn parse_data(lines: &Vec<String>, delim: &str) -> Box<Vec<Vec<String>>> {
-    let mut data: Vec<Vec<String>> = Vec::new();
-
-    for line in lines {
-        let vec = line.split(delim).map(String::from).collect();
-        data.push(vec);
-    }
-
-    Box::new(data)
-}
-
-fn run_thread(data: &Vec<String>, delim: &str) -> Box<Vec<Vec<String>>> {
-    if delim == COMMA {
-        return parse_csv(data);
-    }
-
-    parse_data(data, delim)
-}
-
-fn parse_text(data: &str, delim: &str) -> Box<Vec<Vec<String>>> {
-    let lines: Vec<String> = data.lines().map(|g| g.into()).collect();
-    let lsize: usize = lines.len();
-    let csize = lsize / THSIZE;
-    let chunks: Vec<Vec<String>> = lines.chunks(THSIZE).map(|m| m.into()).collect();
-    let mut handles = vec![];
-
-    for i in 0..csize {
-        println!("Chunk={:?}={}", chunks[i], i);
-        let dl = String::from(delim).clone();
-        let ln = lines.clone();
-
-        let handle = thread::spawn(move || {
-            run_thread(&ln, &dl)
-        });
-
-        handles.push(handle);
-    }
-
-    for handle in handles {
-        let result = handle.join().unwrap();
-        println!("Result={:?}", result);
-    }
-
-        println!("Finished:{:?}", "dimcher");
-
-    Box::new(Vec::new())
-}
-
-fn file_array(file: &str) -> Box<Vec<Vec<String>>> {
-    let map: HashMap<&str, &str> = Files::new();
-    let fmt = file_type(file);
-    let data = read_file(file);
-    let delim = file_delim(&fmt, &map);
-
-    parse_text(&data, &delim)
-}
-
-fn conv_data(data: &Vec<Vec<String>>, delim: &str) -> Box<String> {
-    let len = data.len();
-    let mut arr: Vec<String> = Vec::new();
-
-    for i in 0..len {
-        arr.push(data[i].join(&delim.to_string()));
-    }
-    
-    Box::new(arr.join(EOL))
-}
+use std::collections::HashMap;
 
 fn cx_array<'a, C: Context<'a>>(vec: &Vec<Vec<String>>, cx: &mut C) -> JsResult<'a, JsArray> {
     let rows: Handle<JsArray> = cx.empty_array();
@@ -241,7 +52,7 @@ fn readarray(mut cx: FunctionContext) -> JsResult<JsArray> {
 
     let source: String = s.value() as String; 
 
-    let data = file_array(&source);
+    let data = support::file_array(&source);
 
     cx_array(&data, &mut cx)
 }
@@ -251,7 +62,7 @@ fn readobject(mut cx: FunctionContext) -> JsResult<JsArray> {
 
     let source: String = s.value() as String; 
 
-    let data = file_array(&source);
+    let data = support::file_array(&source);
 
     cx_object(&data, &mut cx)
 }
@@ -260,7 +71,7 @@ fn readtext(mut cx: FunctionContext) -> JsResult<JsString> {
     let name: Handle<JsString> = cx.argument(0)?;
     let file: String = name.value() as String;
 
-    let text = read_file(&file);
+    let text = support::read_file(&file);
     
     Ok(cx.string(*text))
 }
@@ -272,13 +83,13 @@ fn writetext(mut cx: FunctionContext) -> JsResult<JsNumber> {
     let file: &str = &o.value();
     let data: &str = &d.value();
 
-    let size = write_file(file, data);
+    let size = support::write_file(file, data);
     
     Ok(cx.number(size as f64))
 }
 
 fn filetypes(mut cx: FunctionContext) -> JsResult<JsObject> {
-    let map: HashMap<&str, &str> = Files::new();
+    let map: HashMap<&str, &str> = support::Files::new();
 
     let obj: Handle<JsObject> = cx.empty_object();
 
@@ -299,14 +110,14 @@ fn convfile(mut cx: FunctionContext) -> JsResult<JsNumber> {
     let source: &str = &i.value(); 
     let target: &str = &o.value();
 
-    let map: HashMap<&str, &str> = Files::new();
+    let map: HashMap<&str, &str> = support::Files::new();
 
-    let data = file_array(&source);
-    let ttype = file_type(&target);
-    let delim = file_delim(&ttype, &map);
+    let data = support::file_array(&source);
+    let ttype = support::file_type(&target);
+    let delim = support::file_delim(&ttype, &map);
 
-    let text = conv_data(&data, &delim);
-    let size: usize = write_file(target, &text);
+    let text = support::conv_data(&data, &delim);
+    let size: usize = support::write_file(target, &text);
 
     Ok(cx.number(size as f64))
 }
@@ -320,15 +131,15 @@ fn convtext<'a>(mut cx: FunctionContext) -> JsResult<JsNumber> {
     let delim: &str = &d.value(); 
     let target: &str = &o.value();
 
-    let map: HashMap<&str, &str> = Files::new();
+    let map: HashMap<&str, &str> = support::Files::new();
 
-    let data = parse_text(text, delim);
+    let data = support::parse_text(text, delim);
 
-    let ttype = file_type(&target);
-    let delim = file_delim(&ttype, &map);
+    let ttype = support::file_type(&target);
+    let delim = support::file_delim(&ttype, &map);
 
-    let text = conv_data(&data, &delim);
-    let size: usize = write_file(target, &text);
+    let text = support::conv_data(&data, &delim);
+    let size: usize = support::write_file(target, &text);
 
     Ok(cx.number(size as f64))
 }
